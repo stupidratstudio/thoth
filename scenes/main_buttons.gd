0c19474a6df73b2f60ff3737cae31a6244b4d41d
c64cosmin <c64cosmin@gmail.com>
2023-02-06 02:49:55 +0200

    Main menu buttons

---
+++ scenes/main_buttons.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_new_level0_pressed():
+	get_tree().change_scene("res://scenes/level0.tscn")
+
+func _on_new_level1_pressed():
+	get_tree().change_scene("res://scenes/level1.tscn")
+
+func _on_load_level0_pressed():
+	pass # Replace with function body.
+
+func _on_load_level1_pressed():
+	pass # Replace with function body.
