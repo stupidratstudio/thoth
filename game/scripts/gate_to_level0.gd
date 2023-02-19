7851f5ad65b51e5c3ce567e7fb7adda69979fcdd
c64cosmin <c64cosmin@gmail.com>
2023-02-19 23:38:39 +0200

    Pre record

--- game/scripts/gate_to_level0.gd
+++ game/scripts/gate_to_level0.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene("res://game/scenes/level0.tscn")
+		get_tree().change_scene("res://game/levels/level0.tscn")
 		PlayerState.last_visited_level = "level1"
--- game/scripts/gate_to_level1.gd
+++ game/scripts/gate_to_level1.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene("res://game/scenes/level1.tscn")
+		get_tree().change_scene("res://game/levels/level1.tscn")
 		PlayerState.last_visited_level = "level0"
